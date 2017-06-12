86e607e5e83153d8622b988552c2e225c148f3af
lsrikar <lsrikar.it@gmail.com>
2017-06-12 14:48:12 +0530

    first change

---
+++ Reviews.R
@@ -0,0 +1,23 @@
+
+# import the data set
+
+dataset = read.delim("Restaurant_Reviews.tsv",quote = '',stringsAsFactors = FALSE)
+
+# cleaning the text
+library(tm)
+library(SnowballC)
+
+Corpus = VCorpus(VectorSource(dataset$Review))
+
+Corpus = tm_map(Corpus,content_transformer(tolower))
+
+Corpus = tm_map(Corpus,removeNumbers)
+
+Corpus = tm_map(Corpus,removePunctuation)
+
+Corpus = tm_map(Corpus,removeWords,stopwords())
+
+Corpus  = tm_map(Corpus,stemDocument)
+
+Corpus = tm_map(Corpus, stripWhitespace)
+
